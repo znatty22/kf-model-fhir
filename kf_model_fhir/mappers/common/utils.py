16eb1415db050441def11fb48c73a0c4803f09b2
liberaliscomputing <liberalis.computing@gmail.com>
2020-03-27 14:57:51 -0400

    :construction: add identifier maker

--- kf_model_fhir/mappers/common/utils.py
+++ kf_model_fhir/mappers/common/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def make_select(eng, table, *args):
     row = next(eng.execute(f'SELECT * FROM {table};'))
     cols = set(a for a in args) & set(row.keys())
@@ -7,3 +10,11 @@ def make_select(eng, table, *args):
 def get(row, col):
     res = dict(row).get(col)
     return None if res == '' else res
+
+# http://hl7.org/fhir/R4/datatypes.html#id
+def make_identifier(*args):
+    return re.sub(
+        r"[^A-Za-z0-9\-\.]",
+        "-",
+        ".".join(str(a) for a in args)
+    )[:64]
